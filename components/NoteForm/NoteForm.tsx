7db963ac54b6050832a085ae3f7bb9f9ecc573c4
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-08-01 12:04:30 +0300

    tsx

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -1,8 +1,8 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { createNote } from "../../services/noteService";
-import css from "../NoteForm/NoteForm.module.css";
+import css from './NoteForm.module.css';
+import { createNote } from "../../lib/api"
 import type { NoteTag } from "../../types/note";
 
 const validationSchema = Yup.object({
