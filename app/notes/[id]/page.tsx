fb84b17ac21c3833531ab3562e4cdc4de3c87ad8
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-08-02 13:03:02 +0300

    tsx

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -4,7 +4,7 @@ import NoteDetailsClient from "./NoteDetails.client";
 import { HydrationBoundary } from "@tanstack/react-query";
 
 interface NoteDetailsProps {
-    params: { id: string };
+    params: Promise<{ id: string }>;
 }
 
 export default async function NoteDetails({ params }: NoteDetailsProps) {
