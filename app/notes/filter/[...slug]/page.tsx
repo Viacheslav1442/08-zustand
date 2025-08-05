da2ea142c06d65cc3038b3e30c4690a13a1f65ac
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-08-05 17:40:11 +0300

    tsx

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,10 +1,17 @@
 import Notes from '../[...slug]/Notes.client';
 
-interface PageProps {
-    params: { slug?: string[] };
-}
 
-export default function NotesPage({ params }: PageProps) {
+type Props = {
+    params: {
+        slug?: string[];
+    };
+};
+
+
+
+
+
+export default function NotesPage({ params }: Props) {
     const tag = params.slug?.[0];
     return <Notes tag={tag} />;
 }
