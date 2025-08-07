61f74a513d5fecc5889f27a87b6f7e5ad4f3eddb
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-08-07 12:43:35 +0300

    tsx

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,18 +1,18 @@
-import type { FetchNoteResponse, NoteTag } from "@/types/note";
+import type { FetchNoteResponse } from "@/types/note";
 import { fetchNotes } from "@/lib/api";
 import NotesClient from "./Notes.client";
 
-export default async function NotesPage({
-    params,
-}: {
+interface PageProps {
     params: { slug?: string[] };
-}) {
+}
+
+export default async function NotesPage({ params }: PageProps) {
     const tag = params.slug?.[0] ?? "All";
 
-
-    const data: FetchNoteResponse = tag === "All"
-        ? await fetchNotes(1, 12, "")
-        : await fetchNotes(1, 12, "", tag);
+    const data: FetchNoteResponse =
+        tag === "All"
+            ? await fetchNotes(1, 12, "")
+            : await fetchNotes(1, 12, "", tag);
 
     return <NotesClient initialData={data} tag={tag} />;
-} 
+}
