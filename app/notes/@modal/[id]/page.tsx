155e36321e12a0802bfb16788c2b9b58e74f62f8
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-08-07 13:40:46 +0300

    tsx

---
+++ app/notes/@modal/[id]/page.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from "react";
+import { fetchNoteById } from "@/lib/api";
+import NotePreview from "../../../../components/NotePreview/NotePreview";
+import styles from "./ModalNote.module.css";
+import { useRouter } from "next/navigation";
+
+type Props = {
+    params: { id: string };
+};
+
+export default async function NoteModalPage({ params }: Props) {
+    const { id } = params;
+
+
+    const note = await fetchNoteById(id);
+
+    return (
+        <div className={styles.modalBackdrop}>
+            <NotePreview note={note} />
+            {/* Кнопка або логіка закриття модалки */}
+        </div>
+    );
+}
---
+++ components/NotePreview/NotePreview.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React from "react";
+import { useRouter } from "next/navigation";
+import styles from "./NotePreview.module.css";
+import type { Note } from "@/types/note";
+
+type Props = {
+    note: Note;
+};
+
+export default function NotePreview({ note }: Props) {
+    const router = useRouter();
+
+    const closeModal = () => {
+        router.back();
+    };
+
+    return (
+        <div className={styles.modalContent}>
+            <button className={styles.closeBtn} onClick={closeModal}>
+                ×
+            </button>
+            <h2>{note.title}</h2>
+            <p>{note.content}</p>
+            {/* додатковий контент нотатки */}
+        </div>
+    );
+}
