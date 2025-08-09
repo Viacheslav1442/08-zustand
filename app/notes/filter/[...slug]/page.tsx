b1471982356131aea010dc7d8ff6f3b7714a3ff3
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-08-09 13:41:54 +0300

    tsx

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -3,11 +3,14 @@ import { fetchNotes } from "@/lib/api";
 import NotesClient from "../[...slug]/Notes.client";
 
 interface PageProps {
-    params: { slug?: string[] };
-}
+    params: Promise<{ slug?: string[] }>;
+};
 
 export default async function Page({ params }: PageProps) {
-    const tag = params.slug?.[0] ?? "All";
+    const resolvedParams = await params;
+    const tag = resolvedParams.slug?.[0] ?? "All";
+
+
 
     const data: FetchNoteResponse =
         tag === "All"
