98fa1c1d3fd31a195066a6450531322a10e01086
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-08-09 14:58:43 +0300

    tsx

--- app/notes/filter/[...slug]/Notes.client.tsx
+++ app/notes/filter/[...slug]/Notes.client.tsx
@@ -2,10 +2,8 @@
 
 import { useState, useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
-import { useDebounce } from "use-debounce";
 import { fetchNotes } from "@/lib/api";
 import type { FetchNoteResponse, NoteTag } from "@/types/note";
-import SearchBox from "../../../../components/SearchBox/SearchBox";
 import Pagination from "../../../../components/Pagination/Pagination";
 import Modal from "../../../../components/Modal/Modal";
 import NoteForm from "../../../../components/NoteForm/NoteForm";
@@ -18,34 +16,22 @@ interface NotesClientProps {
 
 export default function NotesClient({ initialData, tag }: NotesClientProps) {
     const [currentPage, setCurrentPage] = useState(1);
-    const [searchQuery, setSearchQuery] = useState("");
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     const limit = 12;
-    const [debouncedSearchQuery] = useDebounce(searchQuery, 500);
 
     useEffect(() => {
         setCurrentPage(1);
-    }, [tag, debouncedSearchQuery]);
+    }, [tag]);
 
     const { data } = useQuery<FetchNoteResponse>({
-        queryKey: ["notes", debouncedSearchQuery, currentPage, tag],
-        queryFn: () =>
-            fetchNotes(
-                currentPage,
-                limit,
-                debouncedSearchQuery,
-                tag === "All" ? "" : tag
-            ),
+        queryKey: ["notes", currentPage, tag],
+        queryFn: () => fetchNotes(currentPage, limit, tag === "All" ? "" : tag),
         initialData,
         placeholderData: () => initialData,
     });
 
     const totalPages = Math.ceil((data?.total ?? 0) / limit);
-
-    const handleSearch = (value: string) => {
-        setSearchQuery(value);
-    };
 
     const handlePageChange = (page: number) => {
         setCurrentPage(page);
@@ -55,22 +41,12 @@ export default function NotesClient({ initialData, tag }: NotesClientProps) {
         <div>
             <h2>Notes filtered by &quot;{tag}&quot;</h2>
 
-            <SearchBox value={searchQuery} onChange={handleSearch} />
-
             <button onClick={() => setIsModalOpen(true)}>Add Note</button>
 
-            {data?.notes?.length ? (
-                <NoteList notes={data.notes} />
-            ) : (
-                <p>No notes found.</p>
-            )}
+            {data?.notes?.length ? <NoteList notes={data.notes} /> : <p>No notes found.</p>}
 
             {totalPages > 1 && (
-                <Pagination
-                    currentPage={currentPage}
-                    totalPages={totalPages}
-                    onPageChange={handlePageChange}
-                />
+                <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
             )}
 
             {isModalOpen && (
