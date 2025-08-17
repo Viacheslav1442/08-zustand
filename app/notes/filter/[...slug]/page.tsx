d130ac27b4ec2aa67ba8d9b6cb8015b1d5341c91
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-08-17 12:51:43 +0300

    tsx

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -8,23 +8,25 @@ type Props = {
 };
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-    const { slug } = await params;
+    const resolvedParams = await params;
+    const tag = resolvedParams.slug[0] === "All" ? undefined : resolvedParams.slug[0];
 
     return {
-        title: `${slug[0]} notes`,
-        description: `List of your notes filtered by tag ${slug[0]}. Ability to view, edit and delete notes. Create a new note`,
+        title: `Notes: ${tag || "All"}`,
+        description: `List of notes filtered by tag: ${tag || "All"}. Ability to view, edit and delete notes. Create a new note.`,
         openGraph: {
-            title: `${slug[0]} notes`,
-            description: `List of your notes filtered by tag ${slug[0]}. Ability to view, edit and delete notes. Create a new note`,
-            url: `https://08-zustand-blue.vercel.app/notes/filter/${slug[0]}`,
+            title: `Notes: ${tag || "All"}`,
+            description: `List of notes filtered by tag: ${tag || "All"}. Ability to view, edit and delete notes. Create a new note.`,
+            url: `https://08-zustand-blue.vercel.app/notes/filter/${tag || "All"}`,
             images: [
                 {
                     url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
                     width: 1200,
                     height: 630,
-                    alt: `${slug[0]} notes`,
-                },
+                    alt: `Notes: ${tag || "All"}`,
+                }
             ],
+            type: 'article',
         },
     };
 }
@@ -33,13 +35,15 @@ export default async function FilteredNotesPage({ params }: Props) {
     const resolvedParams = await params;
     const tag = resolvedParams.slug[0] === "All" ? undefined : resolvedParams.slug[0];
 
+    let initialData: FetchNotesResponse;
 
-    const initialData: FetchNotesResponse = await fetchNotes(
-        1,    // page
-        12,   // perPage
-        "",   // search
-        tag   // tag
-    );
+    try {
+        initialData = await fetchNotes(1, 12, "", tag);
+    } catch (err) {
+        console.error("fetchNotes failed:", err);
 
-    return <NotesClient initialData={initialData} tag={tag || 'All'} />;
+        initialData = { total: 0, notes: [], totalPages: 0, page: 1, perPage: 12 };
+    }
+
+    return <NotesClient initialData={initialData} tag={tag || "All"} />;
 }
