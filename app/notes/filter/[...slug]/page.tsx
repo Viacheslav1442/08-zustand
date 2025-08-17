00768d7a7253fa1166344ff8777bea2f22dc6311
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-08-17 17:20:52 +0300

    tsx

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -38,7 +38,7 @@ export default async function Page({ params }: Props) {
     const data: FetchNotesResponse = await fetchNotes(
         1,
         12,
-        "",  // search порожній
+        "",
         tag !== "All" ? tag : undefined
     );
 
