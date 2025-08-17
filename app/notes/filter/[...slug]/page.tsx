67ea6bc587422478cfb5db0e1fdb08d103b2612e
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-08-17 12:46:51 +0300

    tsx

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -8,26 +8,24 @@ type Props = {
 };
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-    const resolvedParams = await params;
-    const tag = resolvedParams.slug[0] === "All" ? undefined : resolvedParams.slug[0];
+    const { slug } = await params;
 
     return {
-        title: `Notes: ${tag || "All"}`,
-        description: `${tag || "All"} notes list`,
+        title: `${slug[0]} notes`,
+        description: `List of your notes filtered by tag ${slug[0]}. Ability to view, edit and delete notes. Create a new note`,
         openGraph: {
-            title: `Notes: ${tag || "All"}`,
-            description: `${tag || "All"} notes list`,
-            url: `https://08-zustand-ten-kappa.vercel.app/notes/filter/${tag || "All"}`,
+            title: `${slug[0]} notes`,
+            description: `List of your notes filtered by tag ${slug[0]}. Ability to view, edit and delete notes. Create a new note`,
+            url: `https://08-zustand-blue.vercel.app/notes/filter/${slug[0]}`,
             images: [
                 {
-                    url: 'https://ac.goit.global/fullstack/react/og-meta.jpg',
+                    url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
                     width: 1200,
                     height: 630,
-                    alt: "NoteHub logo",
-                }
+                    alt: `${slug[0]} notes`,
+                },
             ],
-            type: 'article',
-        }
+        },
     };
 }
 
